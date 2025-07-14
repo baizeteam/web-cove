08366c74f14e27a640cc09b78b0b292d0d7b7a2a
coderwhytop <1795691637@qq.com>
2025-07-14 21:04:42 +0800

    feat: 利用 gpt 封装一些组件

---
+++ src/components/Views/ChoiceQuestion/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <ChoiceQuestion />
+    <div style="height: 40px"></div>
+    <!-- 用于模拟页面空间，使底部组件在下方显示 -->
+    <BottomIconBar />
+  </div>
+</template>
+
+<script setup>
+import ChoiceQuestion from "./ChoiceQuestion.vue";
+import BottomIconBar from "./BottomIconBar.vue";
+</script>
+
+<style scoped>
+/* 可根据需要补充页面样式 */
+</style>
