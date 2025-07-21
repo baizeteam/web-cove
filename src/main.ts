a480417b9a531b2341b6adb022eeb598c00a9a6c
coderwhytop <1795691637@qq.com>
2025-07-21 17:30:48 +0800

    feat: 增加 Eruda 日志排查工具

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { createPinia } from "pinia";
 import router from "./router";
 // import '@/assets/styles/index.less'
 import plugins from "@/plugins";
+import { initEruda } from "@/utils/logger.util.ts";
 
+initEruda(); // 初始化 Eruda
 const app = createApp(App);
 app.use(createPinia());
 app.use(router);
--- src/utils/logger.util.ts
+++ src/utils/logger.util.ts
@@ -1,5 +1,7 @@
 import { isObjectLike, safeStringify } from "@/utils/helper.util.ts";
+import eruda from "eruda";
 
+// 普通的带颜色 加粗console.log函数
 export function colorLog(...args: any[]) {
   args.forEach((arg) => {
     // 安全处理对象转字符串（避免循环引用报错）
@@ -11,3 +13,17 @@ export function colorLog(...args: any[]) {
     );
   });
 }
+
+// 移动端集成调试工具
+export const initEruda = () => {
+  if (import.meta.env.DEV) {
+    // 仅在开发环境启用
+    const el = document.createElement("div");
+    el.classList.add("__eruda_console");
+    document.body.appendChild(el);
+    eruda.init({
+      container: el,
+      tool: ["console", "elements", "network", "resources", "sources"], // 按需选择功能
+    });
+  }
+};
